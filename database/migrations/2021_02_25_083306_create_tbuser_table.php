5d9d70c4df27a4d1704e3856bb97b562e554d9fc
= <=>
2021-03-24 18:27:59 +0800

    init

---
+++ database/migrations/2021_02_25_083306_create_tbuser_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Database\tbuser;
+use App\Database\tbpemilik;
+use App\Utils\makeid;
+
+class CreateTbuserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tbuser', function (Blueprint $table) {
+            $table->string('id_user', 10)->primary();
+            $table->string('username')->unique();
+            $table->enum('role',['admin', 'pemilik','wisatawan']);
+            $table->string('password');
+            $table->enum('status',['enable', 'disable'])
+            $table->timestamps();
+        });
+
+        tbuser::create([
+            'id_user'   => makeid::createId(10),
+            'username'  => 'dika',
+            'role'      => 'Admin',
+            'password'  => Hash::make('123456')
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tbuser');
+    }
+}
