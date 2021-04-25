b700e401290ddbc03f0ba1347bf52ff44c9c4b63
= <=>
2021-04-26 00:17:20 +0800

    pengguna progres

---
+++ resources/views/Page/Pengguna/home.blade.php
@@ -0,0 +1,23 @@
+@extends('Page.Pengguna.master')
+@section('content')
+<!DOCTYPE html>
+<html>
+
+<body>
+    <div class="row">
+        <div class="col-6">
+            <div class="card">
+                <div class="card-body">
+                    <h4>list villa</h4>
+                </div>
+            </div>
+        </div>
+        <div class="col-6">
+            <div class="card">
+                <div class="card-body">
+                    <h4>buat map</h4>
+                </div>
+            </div>
+        </div>
+
+@endsection
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -13,25 +13,27 @@
 		  <div class="card-title text-uppercase text-center py-3">Sign In</div>
             <form action="{{route('login')}}" method="POST">
                 {{ csrf_field() }}
-			  <div class="form-group">
-			  <label for="exampleInputUsername" class="">Username</label>
-			   <div class="position-relative has-icon-right">
-				  <input type="text" name="username" id="exampleInputUsername" class="form-control input-shadow" placeholder="Enter Username">
-				  <div class="form-control-position">
-					  <i class="icon-user"></i>
-                  </div>
+                <div class="form-group">
+                    <label for="exampleInputUsername" class="">Username</label>
+                    <div class="position-relative has-icon-right">
+                        <input type="text" name="username" id="exampleInputUsername" class="form-control input-shadow"
+                            placeholder="Enter Username">
+                        <div class="form-control-position">
+                            <i class="icon-user"></i>
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputPassword" class="">Password</label>
+                    <div class="position-relative has-icon-right">
+                        <input type="password" name="password" id="exampleInputPassword" class="form-control input-shadow"
+                            placeholder="Enter Password">
+                        <div class="form-control-position">
+                            <i class="icon-lock"></i>
+                        </div>
+                    </div>
+                </div>
 
-			   </div>
-			  </div>
-			  <div class="form-group">
-			  <label for="exampleInputPassword" class="">Password</label>
-			   <div class="position-relative has-icon-right">
-				  <input type="password" name="password" id="exampleInputPassword" class="form-control input-shadow" placeholder="Enter Password">
-				  <div class="form-control-position">
-					  <i class="icon-lock"></i>
-				  </div>
-			   </div>
-			  </div>
 
 			 <button type="submit" class="btn btn-primary shadow-primary btn-block waves-effect waves-light">Sign In</button>
 
